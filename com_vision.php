0efef584edab76d1fe9b3cadef66e5ac13c7a70d
Choirunnisa1 <nisaninda3@gmail.com>
2019-04-05 22:19:41 +0700

    five

--- com_vision.php
+++ com_vision.php
@@ -1,36 +1,4 @@
 <?php
-/**----------------------------------------------------------------------------------
-* Microsoft Developer & Platform Evangelism
-*
-* Copyright (c) Microsoft Corporation. All rights reserved.
-*
-* THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-* OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-*----------------------------------------------------------------------------------
-* The example companies, organizations, products, domain names,
-* e-mail addresses, logos, people, places, and events depicted
-* herein are fictitious.  No association with any real company,
-* organization, product, domain name, email address, logo, person,
-* places, or events is intended or should be inferred.
-*----------------------------------------------------------------------------------
-**/
-
-/** -------------------------------------------------------------
-# Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
-# Blob storage stores unstructured data such as text, binary data, documents or media files. 
-# Blobs can be accessed from anywhere in the world via HTTP or HTTPS. 
-#
-# Documentation References: 
-#  - Associated Article - https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-php 
-#  - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
-#  - Getting Started with Blobs - https://azure.microsoft.com/en-us/documentation/articles/storage-php-how-to-use-blobs/
-#  - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
-#  - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx 
-#  - Blob Service PHP API - https://github.com/Azure/azure-storage-php
-#  - Storage Emulator - http://azure.microsoft.com/en-us/documentation/articles/storage-use-emulator/ 
-#
-**/
 
 require_once 'vendor/autoload.php';
 require_once "./random_string.php";
@@ -41,12 +9,12 @@ use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=dicodingappstoragechoi;AccountKey=K9ESSoA0qj1KvBt8y0FkMNvCkMVhvX7GTPtjbuQ3qEeGS4MNOWg+mwGTs7zXB7yBLojHxQdKHauTQMitOOfr1g==;EndpointSuffix=core.windows.net";
 
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
-$fileToUpload = "HelloWorld.txt";
+$fileToUpload = "image.jpg";
 
 if (!isset($_GET["Cleanup"])) {
     // Create container options object.
@@ -72,7 +40,7 @@ if (!isset($_GET["Cleanup"])) {
     $createContainerOptions->addMetaData("key1", "value1");
     $createContainerOptions->addMetaData("key2", "value2");
 
-      $containerName = "blockblobs".generateRandomString();
+      $containerName = "blockblobsnisa".generateRandomString();
 
     try {
         // Create container.
@@ -84,7 +52,7 @@ if (!isset($_GET["Cleanup"])) {
         
         # Upload file as a block blob
         echo "Uploading BlockBlob: ".PHP_EOL;
-        echo $fileToUpload;
+        echo "image.jpg";
         echo "<br />";
         
         $content = fopen($fileToUpload, "r");
@@ -94,7 +62,7 @@ if (!isset($_GET["Cleanup"])) {
 
         // List blobs.
         $listBlobsOptions = new ListBlobsOptions();
-        $listBlobsOptions->setPrefix("HelloWorld");
+        $listBlobsOptions->setPrefix("");
 
         echo "These are the blobs present in the container: ";
 
@@ -111,8 +79,8 @@ if (!isset($_GET["Cleanup"])) {
 
         // Get blob.
         echo "This is the content of the blob uploaded: ";
-        $blob = $blobClient->getBlob($containerName, $fileToUpload);
-        fpassthru($blob->getContentStream());
+        // $blob = $blobClient->getBlob($containerName, $fileToUpload);
+        // fpassthru($blob->getContentStream());
         echo "<br />";
     }
     catch(ServiceException $e){
@@ -163,7 +131,7 @@ else
 
 <head>
     <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="/jquery.min.js"></script>
 </head>
 
 <body>
